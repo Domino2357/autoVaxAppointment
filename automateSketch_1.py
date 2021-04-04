50046580e1e73f36e77ba32e9c631b093963f76d
Domino2357 <JustinBuerger@googlemail.com>
2021-04-04 19:52:08 +0200

    Put in post code

--- automateSketch_1.py
+++ automateSketch_1.py
@@ -4,32 +4,42 @@ from selenium.webdriver.common.by import By
 
 def main():
     driver = webdriver.Firefox()
-    driver.get("https://www.impfportal-niedersachsen.de/portal/#/appointment/public")
+    driver.get('https://www.impfportal-niedersachsen.de/portal/#/appointment/public')
 
     # Page 1: Datenschutzerklärung
     # Check Datenschutzerklärung
-    checkbox = driver.find_element(By.ID, "mat-checkbox-1")
+    checkbox = driver.find_element(By.ID, 'mat-checkbox-1')
     checkbox.click()
-    # Click Weiter
-    continue_data = driver.find_element_by_xpath("/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]/span[1]")
+    # Click continue (Weiter)
+    continue_data = driver.find_element_by_xpath('/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]/span[1]')
     continue_data.click()
 
     # Page 2: Choose appointment type
     # "Einzeltermin" is already checked, therefore, continue
-    continue_appointment_type = driver.find_element_by_xpath("/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]")
+    continue_appointment_type = driver.find_element_by_xpath('/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]')
     continue_appointment_type.click()
 
     # Page 3: put in date of birth for selection of vaccination priority group
     # please note: above 70 year olds are automatically checked
     input_date_of_birth = driver.find_element_by_xpath('//*[@id="mat-input-2"]')
-    input_date_of_birth.send_keys("01.01.1950")
+    input_date_of_birth.send_keys('01.01.1950')
     # Group is automatically checked, therefore, continue
-    continue_date_of_birth = driver.find_element_by_xpath("/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]/span[1]")
+    continue_date_of_birth = driver.find_element_by_xpath('/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]/span[1]')
     continue_date_of_birth.click()
 
     # Page 4: Stiko Indikation - information regarding the vaccinating rule framework in Germany
-    continue_stiko = driver.find_element_by_xpath("/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]")
+    continue_stiko = driver.find_element_by_xpath('/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]')
     continue_stiko.click()
+
+    # Page 5: specify post code and see if there free appointments
+    # put post code (Postleitzahl)
+    input_post_code = driver.find_element_by_xpath('//*[@id="mat-input-0"]')
+    # post code for "Leer"
+    input_post_code.send_keys('26789')
+    # confirm search (Suchen)
+    confirm_search = driver.find_element_by_xpath('/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[1]/div/div[1]/div[3]/div/button/span[1]')
+    confirm_search.click()
+
 
 if __name__ == '__main__':
     main()
