316eec45b88e6a49bb24cc7d3a167ecdb2e456aa
Domino2357 <JustinBuerger@googlemail.com>
2021-04-04 18:36:35 +0200

    Continue with preselected appointment type.

--- automateSketch_1.py
+++ automateSketch_1.py
@@ -6,9 +6,20 @@ def main():
     driver = webdriver.Firefox()
     driver.get("https://www.impfportal-niedersachsen.de/portal/#/appointment/public")
 
+    # Page 1: Datenschutzerklärung
     # Check Datenschutzerklärung
     checkbox = driver.find_element(By.ID, "mat-checkbox-1")
     checkbox.click()
+    # Click Weiter
+    continue_data = driver.find_element_by_xpath("/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]/span[1]")
+    continue_data.click()
+
+    # Page 2: Choose appointment type
+    # "Einzeltermin" is already checked, therefore, continue
+    continue_appointment_type = driver.find_element_by_xpath("/html/body/my-app/div/div[3]/mat-sidenav-container/mat-sidenav-content/appointment-public-view/div/form/div[2]/div/button[2]")
+    continue_appointment_type.click()
+
+
 
 
 if __name__ == '__main__':
