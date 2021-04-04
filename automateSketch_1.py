8d32507481f9fa36326bf60f4d930f0a481937fb
Domino2357 <JustinBuerger@googlemail.com>
2021-04-04 18:23:41 +0200

    First input checkbox works

--- automateSketch_1.py
+++ automateSketch_1.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def main():
     driver = webdriver.Firefox()
     driver.get("https://www.impfportal-niedersachsen.de/portal/#/appointment/public")
+
+    # Check Datenschutzerklärung
+    checkbox = driver.find_element(By.ID, "mat-checkbox-1")
+    checkbox.click()
 
 
 if __name__ == '__main__':
